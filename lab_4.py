a35184302a8879788a443ced3a7e7c0d98214e4e
DmitriyGolubnichiy <jorusfire@gmail.com>
2023-10-14 22:32:56 +0300

    lab 2 3 4 5

--- lab_4.py
+++ lab_4.py
@@ -1,20 +1,21 @@
 
-import datetime
+def WednesdayTheThird():
+    import datetime
 
-from datetime import timedelta
+    from datetime import timedelta
+    current_date = datetime.datetime.now()
 
-current_date = datetime.datetime.now()
+    curr_year = current_date.year
 
-curr_year = current_date.year
+    curr_month = current_date.month
 
-curr_month = current_date.month
+    dayOfMonth = datetime.datetime(curr_year , curr_month , 1)
 
-dayOfMonth = datetime.datetime(curr_year , curr_month , 1)
+    while dayOfMonth.weekday() != 2:
+        dayOfMonth += timedelta(days=1)
 
-while dayOfMonth.weekday() != 2:
-    dayOfMonth += timedelta(days=1)
+    thirdWed = dayOfMonth + timedelta(weeks=2)
 
-thirdWed = dayOfMonth + timedelta(weeks=2)
-
-print(thirdWed)
+    return(thirdWed)
+print(WednesdayTheThird())
 
